a8bbd475290c396d058474ca2ba1dd77a4372efc
Chisadin <tranthanhnam.ccc@gmail.com>
2025-12-19 14:25:24 +0700

    feat: add registration-related functions and types for room and equipment booking

--- src/services/elibService.ts
+++ src/services/elibService.ts
@@ -1,4 +1,6 @@
-import { DANGKY_LichDangKyTheoNgay_Response, DANGKY_PhongHocNhomSelect_Response, LichCaNhanAPIResponse, ThongSo } from "@/types/elib"
+import { DANGKY_LichDangKyTheoNgay_Response, DANGKY_PhongHocNhomForRegSelectResponse, 
+    DANGKY_PhongHocNhomSelect_Response, DANGKY_ThietBiForRegSelectResponse, 
+    LichCaNhanAPIResponse, ThongSo, DangKyPayload } from "@/types/elib"
 import { AuthStorage } from "@/types/user"
 
 const TAPI = import.meta.env.VITE_LHU_TAPI
@@ -132,6 +134,109 @@ export const ELIB_SERVICE = {
                 return null;
             }
             return null;
+        }
+    },
+    // REG area
+    get_phong_hoc_for_reg: async (startTime: string, endTime: string): Promise<DANGKY_PhongHocNhomForRegSelectResponse | null> => {
+        if (!startTime || !endTime) return null
+        const access_token = AuthStorage.getUserToken()
+        if (!access_token) return null
+
+        try {
+            const res = await fetch(`${TAPI}/elib/DANGKY_PhongHocNhomForRegSelect`, {
+                method: "POST",
+                headers: {
+                    "Authorization": `Bearer ${access_token}`,
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    ThoiGianBD: startTime,
+                    ThoiGianKT: endTime
+                })
+            })
+            if (!res.ok) {
+                if ((await res.json())?.Message === "Chứng thực của bạn không hợp lệ") {
+                    AuthStorage.deleteUser()
+                    throw new Error("Token hết hạn, vui lòng đăng nhập lại")
+                }
+                throw new Error(`Lỗi khi lấy Phòng Học Cho Đăng Ký: ${res.status} ${res.statusText}`)
+            }
+            const data: DANGKY_PhongHocNhomForRegSelectResponse = await res.json()
+            return data
+        } catch (e) {
+            if (e instanceof Error) {
+                console.error("Error fetching Phong Hoc For Reg:", e.message);
+                return null;
+            }
+            return null;
+        }
+    },
+    get_thiet_bi_for_reg: async (ThoiGianBD: string, ThoiGianKT: string): Promise<DANGKY_ThietBiForRegSelectResponse | null> => {
+        const access_token = AuthStorage.getUserToken()
+        if (!access_token) return null
+        if (!ThoiGianBD || !ThoiGianKT) return null
+        try {
+            const res = await fetch(`${TAPI}/elib/DANGKY_ThietBiForRegSelect`, {
+                method: "POST",
+                headers: {
+                    "Authorization": `Bearer ${access_token}`,
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    ThoiGianBD: ThoiGianBD,
+                    ThoiGianKT: ThoiGianKT
+                })
+            })
+            if (!res.ok) {
+                if ((await res.json())?.Message === "Chứng thực của bạn không hợp lệ") {
+                    AuthStorage.deleteUser()
+                    throw new Error("Token hết hạn, vui lòng đăng nhập lại")
+                }
+                throw new Error(`Lỗi khi lấy Thiết Bị Cho Đăng Ký: ${res.status} ${res.statusText}`)
+            }
+            const data: DANGKY_ThietBiForRegSelectResponse = await res.json()
+            return data
+
+        } catch (e) {
+            if (e instanceof Error) {
+                console.error("Error fetching Thiet Bi For Reg:", e.message);
+                return null;
+            } 
+            return null;
+        }
+    },
+    dang_ky_phong_hoc_nhom: async (
+        payload: DangKyPayload
+    ) => {
+
+        const access_token = AuthStorage.getUserToken()
+        if (!access_token) return { success: false, message: "No access token" }
+
+        try {
+            const res = await fetch(`${TAPI}/elib/DANGKY_PhongHocNhom`, {
+                method: "POST",
+                headers: {
+                    "Authorization": `Bearer ${access_token}`,
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(payload)
+            })
+
+            if (!res.ok) {
+                if ((await res.json())?.Message === "Chứng thực của bạn không hợp lệ") {
+                    AuthStorage.deleteUser()
+                    throw new Error("Token hết hạn, vui lòng đăng nhập lại")
+                }
+                throw new Error(`Lỗi khi đăng ký Phòng Học Nhóm: ${res.status} ${res.statusText}`)
+            }
+
+            return { success: true, message: "Đăng ký phòng học nhóm thành công", madatcho: (await res.json()).data[0].DangKyID }
+        } catch (e) {
+            if (e instanceof Error) {
+                console.error("Error in dang_ky_phong_hoc_nhom:", e.message);
+                return { success: false, message: e.message }
+            }
+            return { success: false, message: "Unknown error" }
         }
     }
 }
--- src/types/elib.ts
+++ src/types/elib.ts
@@ -75,3 +75,83 @@ export interface LichCaNhanAPIResponse {
   ];
 }
 
+// Data for reg
+enum BusyStatus {
+  Free = 0,
+  Busy = 1,
+}
+
+interface PhongHoc {
+  PhongID: number;
+  TenPhong: string;
+  SucChuaMin: number;
+  SucChuaMax: number;
+  TrangThai: boolean;
+  GhiChu: string;
+  isBusy: BusyStatus;
+}
+
+export interface DANGKY_PhongHocNhomForRegSelectResponse {
+  data: PhongHoc[];
+}
+
+export interface ToolForReg {
+  ThietBiID: number;
+  TenThietBi: string;
+  GhiChu: string;
+  SoLuong: number;
+  SoLuongDaMuon: number;
+}
+
+export interface DANGKY_ThietBiForRegSelectResponse {
+  data: ToolForReg[];
+}
+
+
+export interface DangKyPayload {
+  DangKyID: string
+  PhongID: number
+  ThoiGianBD: string
+  ThoiGianKT: string
+  jsonThietBi: DangKiThietBi[] // rỗng cũng ok
+  GhiChu: string
+}
+
+
+export interface DangKiThietBi {
+  ThietBiID: number;
+  TenThietBi: string;
+  GhiChu: string;
+  SoLuong: number;
+  SoLuongDKMuon: number; 
+  SoLuongDaMuon: number;
+}
+
+// Tuỳ chỉnh phòng
+
+export interface UserSearch {
+  ObjectID: string;
+  ObjectName: string;
+  ObjectNickName: string;
+  Description: string;
+  avatar: string; // Phải load cái này qua blob vì API buộc auth qua cookie mà ta ở trên different domain
+}
+
+export interface ShareSearchObject {
+  data: UserSearch[];
+}
+
+export interface UserObjFromDK_DocGia_SelectByDangKyID {
+  DangKyID: string;
+  StudentID: string;
+  FirstName: string;
+  LastName: string;
+  Email: string | null;
+  ThoiGianXacNhan: string | null;
+  ImageURL: string;
+  DepartmentName: string;
+}
+
+export interface DANGKY_DocGiaSelectByDangKyID {
+  data: UserObjFromDK_DocGia_SelectByDangKyID[];
+}
